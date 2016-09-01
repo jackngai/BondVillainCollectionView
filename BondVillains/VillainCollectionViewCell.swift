be8db3e49549755e04d6822800979ee62de1e291
Jack Ngai <jackngai@gmail.com>
2016-09-01 17:43:05 -0400

    Initial Commit

---
+++ BondVillains/VillainCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  VillainCollectionViewCell.swift
+//  BondVillains
+//
+//  Created by Jack Ngai on 9/1/16.
+//  Copyright © 2016 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class VillainCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var villainImage: UIImageView!
+    @IBOutlet weak var villainName: UILabel!
+    
+    
+}
